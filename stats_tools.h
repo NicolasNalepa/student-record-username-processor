317b442685b03207263b0f18032593ca9b3dc44a
Nicolas Nalepa <42wmqshjh6@privaterelay.appleid.com>
2026-09-13 15:38:49 -0400

    Expose statistics utilities

---
+++ stats_tools.h
@@ -0,0 +1,11 @@
+#ifndef STATS_TOOLS_H
+#define STATS_TOOLS_H
+
+#include "student_processor.h"
+
+// count how many students have a first name that starts with the given letter 
+
+int count_names_by_letter(char student_array[][MAX_LENGTH], int no_of_students,  char letter);
+
+
+#endif
